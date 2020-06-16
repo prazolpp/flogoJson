9d77e4505997cc64d9d3aa984e06c23c4a52b9f5
Prajwal Pyakurel <prazol.pp@gmail.com>
2020-06-16 14:54:16 -0700

    modified input parameterserver and the code to match the server

--- hooks/flogo-generator.js
+++ hooks/flogo-generator.js
@@ -1,25 +1,27 @@
 const getProtocol = require("./protocols");
-
-const flogoGenerator = (asyncapi, resourceType, protocol) => {
-  if (getProtocol(protocol)) {
-    const generateFlogo = getProtocol(protocol);
+const FLOGO_APP_MODEL = "1.1.0";
+const flogoGenerator = (asyncapi, resourceType, server) => {
+  let currentProtocol = getProtocol(asyncapi.server(server).protocol());
+  if (currentProtocol) {
+    const generateFlogo = currentProtocol;
     const { imports, triggers, resources } = generateFlogo(
       asyncapi,
       resourceType
     );
-
     return {
       name: asyncapi.info().title(),
       type: "flogo:app",
-      version: "0.0.1",
-      appModel: asyncapi.info().version(),
+      version: asyncapi.info().version(),
+      appModel: FLOGO_APP_MODEL,
       description: asyncapi.info().description().split(/\n/g)[0],
       imports,
       triggers,
       resources,
     };
   }
-  throw new Error(`Protocol "${protocol}" is not supported by this template.`);
+  throw new Error(
+    `Protocol "${currentProtocol}" is not supported by this template.`
+  );
 };
 
 module.exports = flogoGenerator;
--- hooks/flogo.js
+++ hooks/flogo.js
@@ -5,21 +5,17 @@ const flogoGenerator = require("./flogo-generator.js");
 module.exports = {
   "generate:before": (generator) => {
     let resourceType = "flow";
-    let protocol = "";
+    let server = "";
     if (
       generator.templateParams &&
       generator.templateParams["resourceType"] === "stream"
     ) {
       resourceType = "stream";
     }
-    if (generator.templateParams && generator.templateParams["protocol"]) {
-      protocol = generator.templateParams["protocol"];
+    if (generator.templateParams && generator.templateParams["server"]) {
+      server = generator.templateParams["server"];
     }
-    const flogoJSON = flogoGenerator(
-      generator.asyncapi,
-      resourceType,
-      protocol
-    );
+    const flogoJSON = flogoGenerator(generator.asyncapi, resourceType, server);
     fs.writeFileSync(
       path.resolve(generator.targetDir, `flogo.json`),
       JSON.stringify(flogoJSON, null, 2)
